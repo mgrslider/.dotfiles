96b9898d1dbadda7750ca65ab3a1b54bfc71ec9c
mgrillcoder <marcin.grabowski@proton.me>
2026-08-05 00:38:27 +0200

    cpu fixes

--- i3/.config/i3blocks/scripts/volume_watch.sh
+++ i3/.config/i3blocks/scripts/volume_watch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-pw-mon | grep --line-buffered -E "mod.client|changed" | while read -r _; do
-    pkill -RTMIN+10 i3blocks
+pactl subscribe 2>/dev/null | grep --line-buffered "sink" | while read -r _; do
+    now=$(date +%s%3N)
+    if [ $(( now - ${last:-0} )) -ge 200 ]; then
+        pkill -RTMIN+10 i3blocks
+        last=$now
+    fi
 done
